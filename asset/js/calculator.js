e05dba256e8b805c9624f1926ba0a42b4fb1a337
arvinlauron <arvinglauron@gmail.com>
2021-12-14 13:42:56 +0800

    add calculator

---
+++ asset/js/calculator.js
@@ -0,0 +1,19 @@
+var input_box = document.getElementById("calculation");
+
+function UserClickButton(input){
+    var oldinput=input_box.value;
+    var newinput=oldinput+""+input;
+    input_box.value=newinput
+    
+}
+
+function CalculateValue(){
+    var input=input_box.value;
+    var result=eval(input);
+    input_box.value=result;
+   
+}
+
+function ClearData(){
+    input_box.value = "";
+}
